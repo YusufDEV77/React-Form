cb5929e8346d10ad7550d554696461bee2b663a3
YusufDEV77 <gtarprp002@gmail.com>
2021-03-20 10:11:52 +0500

    Updates

---
+++ precache-manifest.4bbe18e345ed944bc37b0791d97f559f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9515e162c2b4546a6cc4ac3ac0eff5ff",
+    "url": "/React-Form/index.html"
+  },
+  {
+    "revision": "d7b08cd40b305e0ec50e",
+    "url": "/React-Form/static/css/main.d2cf0032.chunk.css"
+  },
+  {
+    "revision": "bc6383e3c6fa00ea59fa",
+    "url": "/React-Form/static/js/2.30a0af2a.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/React-Form/static/js/2.30a0af2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b08cd40b305e0ec50e",
+    "url": "/React-Form/static/js/main.d79ccd6e.chunk.js"
+  },
+  {
+    "revision": "4875b6549e3ef2c8b348",
+    "url": "/React-Form/static/js/runtime-main.068d50e5.js"
+  }
+]);
